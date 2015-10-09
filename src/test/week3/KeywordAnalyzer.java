fb7852ed5c84e8a03f93365df666197ba68cad25
Меркушев Игорь <merkushev@iac.spb.ru>
2015-10-09 17:00:23 +0300

    last week 3

---
+++ src/test/week3/KeywordAnalyzer.java
@@ -0,0 +1,8 @@
+package test.week3;
+
+
+public abstract class KeywordAnalyzer {
+    protected abstract String[] getKeywords();
+    protected abstract Label getLabel();
+
+}
--- src/test/week3/TooLongTextAnalyzer.java
+++ src/test/week3/TooLongTextAnalyzer.java
@@ -2,6 +2,11 @@ package test.week3;
 
 
 public class TooLongTextAnalyzer implements TextAnalyzer{
+    private int maxLength;
+
+    public TooLongTextAnalyzer(int maxLength) {
+        this.maxLength = maxLength;
+    }
 
     @Override
     public Label processText(String text) {
