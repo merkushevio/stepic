915a6be0941d15d9c24ffbe2a0db2d3bfcc75775
Igor Merkushev <merkushevio@gmail.com>
2015-10-23 21:54:43 +0300

    week6

--- src/week6/FunctionInterface.java
+++ src/week6/FunctionInterface.java
@@ -19,26 +19,31 @@ Function<String, Integer> safeStringLength = ternaryOperator(condition, ifTrue, 
 *
 * */
 
+import java.util.Objects;
 import java.util.function.Function;
 import java.util.function.Predicate;
 
 public class FunctionInterface {
-    public static void main(String[] args) {
 
+
+    public static void main(String[] args) {
+        Predicate<Object> condition = Objects::isNull;
+        Function<Object, Integer> ifTrue = obj -> 0;
+        Function<CharSequence, Integer> ifFalse = CharSequence::length;
+        Function<String, Integer> safeStringLength = ternaryOperator(condition, ifTrue, ifFalse);
     }
 
+
+
     public static <T, U> Function<T, U> ternaryOperator(
-            Predicate<? super T> condition,
-            Function<? super T, ? extends U> ifTrue,
+            Predicate < ? super T > condition,
+            Function < ? super T, ? extends U> ifTrue,
             Function<? super T, ? extends U> ifFalse) {
 
 
         return null;
     }
 
-    /*    Predicate<Object> condition = Objects::isNull;
-        Function<Object, Integer> ifTrue = obj -> 0;
-        Function<CharSequence, Integer> ifFalse = CharSequence::length;
-        Function<String, Integer> safeStringLength = ternaryOperator(condition, ifTrue, ifFalse);
-    */
+
+
 }
