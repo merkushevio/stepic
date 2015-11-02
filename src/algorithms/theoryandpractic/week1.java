4bb28ccdc2f2e46bdcbae4dea3c891119cc1b1f4
Меркушев Игорь <merkushev@iac.spb.ru>
2015-11-02 09:41:56 +0300

    week1 Algorithm
    week6

---
+++ src/algorithms/theoryandpractic/week1.java
@@ -0,0 +1,16 @@
+package algorithms.theoryandpractic;
+
+import java.util.Scanner;
+
+
+public class week1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int first = scanner.nextInt();
+        int second = scanner.nextInt();
+
+        System.out.println(first + second);
+    }
+
+
+}
